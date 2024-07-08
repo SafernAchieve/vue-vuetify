a0efd90a81dea1d097e16432742938ca29600663
SafernAchieve <126269703+SafernAchieve@users.noreply.github.com>
2024-07-08 15:32:12 -0400

    with Tab

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,42 @@
 <template>
   <v-app id="app">
+    <!-- App bar -->
     <v-app-bar color="primary" dark app>
       <v-toolbar-title>
         <router-link to="/" class="text-decoration-none white--text">Achieve</router-link>
       </v-toolbar-title>
+
+
+
+
+      <v-tabs v-model="tab" background-color="primary">
+  <v-tab v-if="tab === 'one'" value="one">Create Message</v-tab>
+  <v-tab v-else>
+    <router-link to="/about" class="text-decoration-none white--text">Create Message</router-link>
+  </v-tab>
+  
+  <v-tab v-if="tab === 'two'" value="two">View Message</v-tab>
+  <v-tab v-else>
+    <router-link to="/viewMessage" class="text-decoration-none white--text">View Message</router-link>
+  </v-tab>
+
+  <v-tab value="three">Item Three</v-tab>
+</v-tabs>
+
       <v-spacer></v-spacer>
+      <!-- Drawer toggle button -->
       <v-btn icon @click="drawer = !drawer">
         <v-icon>mdi-menu</v-icon>
       </v-btn>
     </v-app-bar>
 
+    <!-- Navigation drawer -->
     <v-navigation-drawer v-model="drawer" app>
       <v-list dense>
         <v-list-item @click="viewMessage">
           <v-list-item-icon>
             <v-icon>mdi-inbox</v-icon>
           </v-list-item-icon>
-
           <v-list-item-content>
             <v-list-item-title>View Message</v-list-item-title>
           </v-list-item-content>
@@ -26,77 +46,32 @@
           <v-list-item-icon>
             <v-icon>mdi-email-plus</v-icon>
           </v-list-item-icon>
-
           <v-list-item-content>
             <v-list-item-title>Create Message</v-list-item-title>
           </v-list-item-content>
         </v-list-item>
-        <v-list-item>
-          <v-list-item-icon>
-            <v-icon>mdi-heart</v-icon>
-          </v-list-item-icon>
-          <v-list-item-content>
-            <v-list-item-title>Item</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-        <v-list-item>
-          <v-list-item-icon>
-            <v-icon>mdi-bricks</v-icon>
-          </v-list-item-icon>
-          <v-list-item-content>
-            <v-list-item-title>Item</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-        <v-list-item>
-          <v-list-item-icon>
-            <v-icon>mdi-clock</v-icon>
-          </v-list-item-icon>
-          <v-list-item-content>
-            <v-list-item-title>Item</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-        <v-list-item>
-          <v-list-item-icon>
-            <v-icon>mdi-archive</v-icon>
-          </v-list-item-icon>
-          <v-list-item-content>
-            <v-list-item-title>Item</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-        <v-list-item>
-          <v-list-item-icon>
-            <v-icon>mdi-cog</v-icon>
-          </v-list-item-icon>
-          <v-list-item-content>
-            <v-list-item-title>Item</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-        <v-list-item>
 
+        <v-list-item @click="Drictory">
           <v-list-item-icon>
-            <v-icon @click="Drictory">mdi-account</v-icon>
+            <v-icon>mdi-account</v-icon>
           </v-list-item-icon>
-
           <v-list-item-content>
-            <v-list-item-title>Item</v-list-item-title>
+            <v-list-item-title>Directory</v-list-item-title>
           </v-list-item-content>
-
-          <v-list-item-action>
-            <v-icon @click="Calendar" >mdi-calendar</v-icon>
-          </v-list-item-action>
         </v-list-item>
 
-        <v-list-item>
+        <v-list-item @click="Calendar">
           <v-list-item-icon>
-            <v-icon>mdi-email</v-icon>
+            <v-icon>mdi-calendar</v-icon>
           </v-list-item-icon>
           <v-list-item-content>
-            <v-list-item-title>Item</v-list-item-title>
+            <v-list-item-title>Calendar</v-list-item-title>
           </v-list-item-content>
         </v-list-item>
       </v-list>
     </v-navigation-drawer>
 
+    <!-- Main content -->
     <v-main>
       <v-container fluid>
         <v-row>
@@ -116,26 +91,32 @@ export default {
   data() {
     return {
       drawer: false,
+      tab: 'one' // Default active tab
     };
   },
   methods: {
     viewMessage() {
-      this.$router.push({ path: '/about' });
+      this.tab = 'one'; // Ensure 'one' tab is active
+      this.$router.push({ path: '/viewMessage' });
     },
     createMessage() {
-      this.$router.push({ path: '/about' });
+      this.tab = 'two'; // Ensure 'two' tab is active
+      this.$router.push({ path: '/createMessage' });
     },
     Drictory() {
-      this.$router.push({ path: '/Directory' });
+      this.tab = 'three'; // Ensure 'three' tab is active
+      this.$router.push({ path: '/directory' });
     },
     Calendar() {
-      this.$router.push({ path: '/Calendar' });
+      this.tab = 'four'; // Ensure 'four' tab is active
+      this.$router.push({ path: '/calendar' });
     },
   },
 };
 </script>
 
 <style scoped>
+/* CSS transitions */
 .fade-enter-active,
 .fade-leave-active {
   transition: opacity 0.9s ease;
